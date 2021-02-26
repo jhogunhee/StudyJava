8704ac70ad3465221d2dd59e703084d8aab905c6
조건희 <rjsgml1677@naver.com>
2021-02-26 13:31:34 +0900

    Sample01 수정 2021-02-26

--- RealJavaGithub/src/Sample01.java
+++ RealJavaGithub/src/Sample01.java
@@ -4,7 +4,7 @@ public class Sample01 {
 		char chr = 'A';
 		System.out.println("char : " + chr); // 문자
 		System.out.println("Encoding : " + (int)chr); // ASCII 코드로 인코딩
-		System.out.println("Decoding : " + (char)65); // 문자로 디코딩dasdsa dsaasd
+		System.out.println("Decoding : " + (char)65); // 문자로 디코딩
 		
 		
 	}
