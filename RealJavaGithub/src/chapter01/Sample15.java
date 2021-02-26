9b229b32b05862bb11cde0e7dc4e41743fed69f2
조건희 <rjsgml1677@naver.com>
2021-02-26 14:33:14 +0900

    타입 추론이 가능한 var를 이용한 자바 예제 var는 자바 10부터 사용 가능 2021-02-25

---
+++ RealJavaGithub/src/chapter01/Sample15.java
@@ -0,0 +1,11 @@
+package chapter01;
+
+public class Sample15 {
+	public static void main(String[] args) {
+		final float PI1;
+		PI1 = 3.14f;
+		
+		// final var PI2; 자료형을 추론할 수 있는 값이 있어야함
+		final var PI3 = 3.14f;
+	}
+}
